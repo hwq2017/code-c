7333dcd5edcd927e567dcb088457a215b598ed7d
hwq <15265036423@163.com>
2017-04-06 13:39:56 +0800

    double linklist

--- link.c
+++ link.c
@@ -2,35 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct BOOK{
-    char title[60];
-    int book_id;
-};
-typedef struct BOOK datatype;
 
+typedef struct{
+    int a;
+    char *b;
+}A;
 
 typedef struct node{
-    datatype data;
-    struct node * next;
+    A data;
+   struct node *prior;
+   struct node *next;
 }linknode,*linklist;
 
 
 linklist link_create()
 {
-    linklist p;
-    p = malloc(sizeof(linknode));
-    if(p == NULL)
+    linklist h;
+    h = malloc(sizeof(linknode));
+    if(h == NULL)
     {
-        printf("malloc failed\n");
+        printf("malloc failed");
         return 0;
     }
-    p->next = NULL;
-    return p;
+    h->prior = h;
+    h->next = h;
+   return h; 
 }
 
 
 
-int link_headerin(linklist p,datatype value)
+/*int link_heardin(linklist h, A value)
 {
     linklist q;
     q = malloc(sizeof(linknode));
@@ -40,47 +41,90 @@ int link_headerin(linklist p,datatype value)
         return 0;
     }
     q->data = value;
-    q->next = NULL;
-    q->next = p->next;
-    p->next = q;
+    q->next = h;
+    q->prior = h;
+    h->next = q;
+    h->prior = q;
     return 0;
-}
+}*/
 
-int link_delete(linklist p)
+
+int link_insert(linklist h, A value)
 {
-   linklist a;
-   a = p->next;
-   p->next = p->next->next;
-   free(a);
-    return 0;
-}
-
-
-void link_show(linklist p)
-{
-    while(p != NULL)
+    linklist q;
+    q = malloc(sizeof(linknode));
+    if(q == NULL)
     {
-        printf("%d %s\n", p->data.book_id, p->data.title);
-        p = p->next;
+        printf("malloc failed\n");
+        return 0;
+    }
+    q->data = value;
+    q->prior = h->prior;
+    q->next = h;
+    h->prior->next = q;
+    h->prior = q;
+    return 0;
+}
+
+
+int link_delete(linklist h, int k)
+{
+   linklist g;
+   g = h ;
+   for(int i = 0; i < k; i ++)
+   {
+       if(g == NULL)
+       {
+           return 1;
+       }
+           g = g->next;
+   }
+   g->prior->next = g->next;
+   g->next->prior = g->prior;
+   free(g);
+   return 0;
+}
+
+
+void link_select(linklist h, int k)
+{
+    linklist e;
+    e = h;
+    for(int i = 0; i < k; i++)
+    {
+        e = e->next;
+    }
+    printf("%d %s\n", e->data.a, e->data.b);
+}
+
+
+
+void link_show(linklist h)
+{
+    linklist f;
+    f = h->next;
+    while(f != h)
+    {
+        printf("%d %s\n", f->data.a, f->data.b);
+        f = f->next;  
     }
 }
 
 
-
-
 int main(int argc, const char *argv[])
 {
-   linklist h = NULL;
-   if((h = link_create()) == NULL)
+   linklist l = NULL;
+   if((l = link_create()) == NULL)
    {
        printf("error\n");
    }
-   datatype b = {"nihao",123}, c = {"hello", 456};
-   link_headerin(h,b);
-   link_headerin(h,c);
-   link_show(h);
-//   link_delete(h);
-
+   A s = {12,"nihao"}, t = {13,"hi"};
+   link_insert(l,s);
+   link_insert(l,t);
+   link_show(l);
+   link_delete(l, 2);
+   printf("***********\n");
+   link_show(l);
+  // link_select(l, 2);
     return 0;
 }
-
